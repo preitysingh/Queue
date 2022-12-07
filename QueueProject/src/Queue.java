6fed0e0a8874beec783dbbd1ba249c0151f5d9c0
preitysingh <111521777+preitysingh@users.noreply.github.com>
2022-12-07 11:44:50 -0700

    3 methods+changes in Node class

--- QueueProject/src/Queue.java
+++ QueueProject/src/Queue.java
@@ -1,2 +1,32 @@
-public class Queue {
+public class Queue <N> {
+    private int size = 0;
+    private QueueNode<N> head = null; //object that is getting popped out next
+    private QueueNode<N> tail= null; //object that just got put in
+    public Queue(){ } //empty constructor
+    
+//    public void enqueue(N element)  {
+//        QueueNode<N> node = new QueueNode<N>(element);
+//      if(tail != null){
+//          tail.setChild = node;
+//      }
+//      tail = node;
+//      if(head == null){head = node;}
+//      size++;
+//    }  //add an element to queue-kinda main
+
+//  public N dequeue()    {
+//    N headData = head.getData;
+//    head = head.getChild();
+//    return headData;
+//    }//remove and return the least recent element
+
+    public boolean isEmpty() {return(head==null);} //at the first pop out position
+    public int size() {return size;}
+    public N peek() {
+        if (isEmpty()) {
+            return null;
+        } else {
+            return head.getData();
+        } //peek which node is next to pop out
+    }
 }
--- QueueProject/src/QueueNode.java
+++ QueueProject/src/QueueNode.java
@@ -1,2 +1,61 @@
-public class QueueNode {
+//preity's node class
+public class QueueNode<N> {
+    //parent,child,data variables
+    private QueueNode parent;
+    private QueueNode child;
+    private N data;
+
+
+    public QueueNode() { } //empty constructor
+
+    public QueueNode (N data){
+        this.data = data; //constructor w/ 1 parameter
+    }
+
+    public QueueNode(QueueNode<N> parent, QueueNode<N> child, N data){
+        //constructor w/ 3 parameters
+        this.parent = parent; //...for Node parent
+        this.child = child;  //...for Node child
+        this.data = data;  //...for data
+    }
+
+    //mutator/setter methods
+    public void setParent(QueueNode<N> parent){
+        this.parent = parent; //reads in parent varible
+    }
+
+    public void setChild(QueueNode<N> child) {
+        this.child = child; //reads in child variable
+    }
+
+    public void setData(N data) {
+        this.data = data; //reads in data variable
+    }
+
+    //accessor methods
+    public QueueNode<N> getParent() {
+        return parent; //returns parent node
+    }
+
+    public QueueNode<N> getChild() {
+        return child; //returns child node
+    }
+
+    public N getData() {
+        return data; //returns data
+    }
+
+    //toString method
+    @Override
+    public String toString() {
+        //toString method for the Node class
+        String str = "Node: " + data;
+        return str;
+    }
+    //.equals method
+    public boolean equals(QueueNode n1) {
+        //checks to see if one node is equal to the other
+        //returns true if equal, false otherwise
+        return(data == n1.getData());
+    }
 }
