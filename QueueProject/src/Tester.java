8abd7a80a350538e0e6809cd5c11201bf9d6d021
preitysingh <111521777+preitysingh@users.noreply.github.com>
2022-12-07 12:30:21 -0700

    adding more to tester

--- QueueProject/src/Tester.java
+++ QueueProject/src/Tester.java
@@ -7,11 +7,15 @@ public class Tester {
         queue.enqueue(3); //adding first element using enqueue
         System.out.println("(false): " + queue.isEmpty()); //testing isEmpty method
         queue.enqueue(24); //adding a second element
-        System.out.println("(2): " + queue.size()); //testing size method
+        queue.enqueue(18);
+        queue.enqueue(9);
+        System.out.println("(4): " + queue.size()); //testing size method
 
         System.out.println("taking out 3: " + queue.dequeue()); //testing dequeue method
         System.out.println("first one put in was 24: " + queue.peek()); //testing peek method
-        System.out.println(queue.dequeue());
+        System.out.println("taking out 24: "+ queue.dequeue());
+        System.out.println("taking out 18: "+queue.dequeue());
+        System.out.println("taking out 9: "+queue.dequeue());
         System.out.println(queue.isEmpty() + ", " + queue.size());
     }
 }
